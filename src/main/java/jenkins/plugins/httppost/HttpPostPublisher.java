7476a3e437a8557479810ca8997f077d832aedac
Li Xia <xautlx@hotmail.com>
2015-09-22 20:04:49 +0800

    Add support to job level parameters configuration

--- src/main/java/jenkins/plugins/httppost/HttpPostPublisher.java
+++ src/main/java/jenkins/plugins/httppost/HttpPostPublisher.java
@@ -34,155 +34,196 @@ import org.kohsuke.stapler.StaplerRequest;
  *
  * @author Christian Becker (christian.becker.1987@gmail.com)
  */
-@SuppressWarnings("UnusedDeclaration") // This class will be loaded using its Descriptor.
+@SuppressWarnings("UnusedDeclaration")
+// This class will be loaded using its Descriptor.
 public class HttpPostPublisher extends Notifier {
-
-  @DataBoundConstructor
-  public HttpPostPublisher() {
-  }
-
-  @SuppressWarnings({"unchecked", "deprecation"})
-  @Override
-  public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-    if (build.getResult().isWorseOrEqualTo(Result.FAILURE)) {
-      listener.getLogger().println("HTTP POST: Skipping because of FAILURE");
-      return true;
-    }
-
-    List<Run.Artifact> artifacts = build.getArtifacts();
-    if (artifacts.isEmpty()) {
-      listener.getLogger().println("HTTP POST: No artifacts to POST");
-      return true;
-    }
-
-    Descriptor descriptor = getDescriptor();
-    String url = descriptor.url;
-    String headers = descriptor.headers;
-    if (url == null || url.length() == 0) {
-      listener.getLogger().println("HTTP POST: No URL specified");
-      return true;
-    }
-
-    try {
-      MultipartBuilder multipart = new MultipartBuilder();
-      multipart.type(MultipartBuilder.FORM);
-      for (Run.Artifact artifact : artifacts) {
-        multipart.addFormDataPart(artifact.getFileName(), artifact.getFileName(),
-            RequestBody.create(null, artifact.getFile()));
-      }
-
-      OkHttpClient client = new OkHttpClient();
-      client.setConnectTimeout(30, TimeUnit.SECONDS);
-      client.setReadTimeout(60, TimeUnit.SECONDS);
-
-      Request.Builder builder = new Request.Builder();
-      builder.url(url);
-      builder.header("Job-Name", build.getProject().getName());
-      builder.header("Build-Number", String.valueOf(build.getNumber()));
-      builder.header("Build-Timestamp", String.valueOf(build.getTimeInMillis()));
-      if (headers != null && headers.length() > 0) {
-        String[] lines = headers.split("\r?\n");
-        for (String line : lines) {
-          int index = line.indexOf(':');
-          builder.header(line.substring(0, index).trim(), line.substring(index + 1).trim());
-        }
-      }
-      builder.post(multipart.build());
-
-      Request request = builder.build();
-      listener.getLogger().println(String.format("---> POST %s", url));
-      listener.getLogger().println(request.headers());
-
-      long start = System.nanoTime();
-      Response response = client.newCall(request).execute();
-      long time = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start);
-
-      listener.getLogger()
-          .println(String.format("<--- %s %s (%sms)", response.code(), response.message(), time));
-      listener.getLogger().println(response.body().string());
-    } catch (Exception e) {
-      e.printStackTrace(listener.getLogger());
-    }
-
-    return true;
-  }
-
-  @Override
-  public BuildStepMonitor getRequiredMonitorService() {
-    return BuildStepMonitor.NONE;
-  }
-
-  @Override
-  public Descriptor getDescriptor() {
-    return (Descriptor) super.getDescriptor();
-  }
-
-  @Extension
-  public static final class Descriptor extends BuildStepDescriptor<Publisher> {
 
     public String url;
     public String headers;
 
-    public Descriptor() {
-      load();
+    @DataBoundConstructor
+    public HttpPostPublisher(String url, String headers) {
+        this.url = url;
+        this.headers = headers;
+    }
+
+    @SuppressWarnings({ "unchecked", "deprecation" })
+    @Override
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+        if (build.getResult().isWorseOrEqualTo(Result.FAILURE)) {
+            listener.getLogger().println("HTTP POST: Skipping because of FAILURE");
+            return true;
+        }
+
+        List<Run.Artifact> artifacts = build.getArtifacts();
+        if (artifacts.isEmpty()) {
+            listener.getLogger().println("HTTP POST: No artifacts to POST");
+            return true;
+        }
+
+        Descriptor descriptor = getDescriptor();
+        if (url == null || url.length() == 0) {
+            url = descriptor.url;
+        }
+        if (headers == null || headers.length() == 0) {
+            headers = descriptor.headers;
+        }
+
+        if (url == null || url.length() == 0) {
+            listener.getLogger().println("HTTP POST: No URL specified");
+            return true;
+        }
+
+        try {
+            MultipartBuilder multipart = new MultipartBuilder();
+            multipart.type(MultipartBuilder.FORM);
+            for (Run.Artifact artifact : artifacts) {
+                multipart.addFormDataPart(artifact.getFileName(), artifact.getFileName(), RequestBody.create(null, artifact.getFile()));
+            }
+
+            OkHttpClient client = new OkHttpClient();
+            client.setConnectTimeout(30, TimeUnit.SECONDS);
+            client.setReadTimeout(60, TimeUnit.SECONDS);
+
+            Request.Builder builder = new Request.Builder();
+            builder.url(url);
+            builder.header("Job-Name", build.getProject().getName());
+            builder.header("Build-Number", String.valueOf(build.getNumber()));
+            builder.header("Build-Timestamp", String.valueOf(build.getTimeInMillis()));
+            if (headers != null && headers.length() > 0) {
+                String[] lines = headers.split("\r?\n");
+                for (String line : lines) {
+                    int index = line.indexOf(':');
+                    builder.header(line.substring(0, index).trim(), line.substring(index + 1).trim());
+                }
+            }
+            builder.post(multipart.build());
+
+            Request request = builder.build();
+            listener.getLogger().println(String.format("---> POST %s", url));
+            listener.getLogger().println(request.headers());
+
+            long start = System.nanoTime();
+            Response response = client.newCall(request).execute();
+            long time = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start);
+
+            listener.getLogger().println(String.format("<--- %s %s (%sms)", response.code(), response.message(), time));
+            listener.getLogger().println(response.body().string());
+        } catch (Exception e) {
+            e.printStackTrace(listener.getLogger());
+        }
+
+        return true;
     }
 
     @Override
-    public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-      return true;
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.NONE;
     }
 
     @Override
-    public String getDisplayName() {
-      return "HTTP POST artifacts to an URL";
+    public Descriptor getDescriptor() {
+        return (Descriptor) super.getDescriptor();
     }
 
     public FormValidation doCheckUrl(@QueryParameter String value) {
-      if (value.length() == 0) {
-        return FormValidation.error("URL must not be empty");
-      }
-
-      if (!value.startsWith("http://") && !value.startsWith("https://")) {
-        return FormValidation.error("URL must start with http:// or https://");
-      }
-
-      try {
-        new URL(value).toURI();
-      } catch (Exception e) {
-        return FormValidation.error(e.getMessage());
-      }
-
-      return FormValidation.ok();
+        return FormValidator.doCheckUrl(value);
     }
 
     public FormValidation doCheckHeaders(@QueryParameter String value) {
-      if (value.length() > 0) {
-        Headers.Builder headers = new Headers.Builder();
-        String[] lines = value.split("\r?\n");
+        return FormValidator.doCheckHeaders(value);
+    }
 
-        for (String line : lines) {
-          int index = line.indexOf(':');
-          if (index == -1) {
-            return FormValidation.error("Unexpected header: " + line);
-          }
+    @Extension
+    public static final class Descriptor extends BuildStepDescriptor<Publisher> {
 
-          try {
-            headers.add(line.substring(0, index).trim(), line.substring(index + 1).trim());
-          } catch (Exception e) {
-            return FormValidation.error(e.getMessage());
-          }
+        public String url;
+        public String headers;
+
+        public Descriptor() {
+            super(HttpPostPublisher.class);
+            load();
         }
-      }
 
-      return FormValidation.ok();
+        @Override
+        public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "HTTP POST artifacts to an URL";
+        }
+
+        public FormValidation doCheckUrl(@QueryParameter String value) {
+            return FormValidator.doCheckUrl(value);
+        }
+
+        public FormValidation doCheckHeaders(@QueryParameter String value) {
+            return FormValidator.doCheckHeaders(value);
+        }
+
+        /**
+         * Handles SiteMonitor configuration for each job.
+         * @param request
+         *            the stapler request
+         * @param json
+         *            the JSON data containing job configuration values
+         * @return the builder with specified sites to be monitored
+         */
+        @Override
+        public final Publisher newInstance(final StaplerRequest request, final JSONObject json) {
+            return new HttpPostPublisher(json.get("url") != null ? json.getString("url") : "",
+                    json.get("headers") != null ? json.getString("headers") : "");
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+            req.bindJSON(this, json.getJSONObject("http-post"));
+            save();
+
+            return true;
+        }
     }
 
-    @Override
-    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
-      req.bindJSON(this, json.getJSONObject("http-post"));
-      save();
+    private static class FormValidator {
 
-      return true;
+        public static FormValidation doCheckUrl(@QueryParameter String value) {
+            if (value.length() > 0) {
+                if (!value.startsWith("http://") && !value.startsWith("https://")) {
+                    return FormValidation.error("URL must start with http:// or https://");
+                }
+
+                try {
+                    new URL(value).toURI();
+                } catch (Exception e) {
+                    return FormValidation.error(e.getMessage());
+                }
+            }
+
+            return FormValidation.ok();
+        }
+
+        public static FormValidation doCheckHeaders(@QueryParameter String value) {
+            if (value.length() > 0) {
+                Headers.Builder headers = new Headers.Builder();
+                String[] lines = value.split("\r?\n");
+
+                for (String line : lines) {
+                    int index = line.indexOf(':');
+                    if (index == -1) {
+                        return FormValidation.error("Unexpected header: " + line);
+                    }
+
+                    try {
+                        headers.add(line.substring(0, index).trim(), line.substring(index + 1).trim());
+                    } catch (Exception e) {
+                        return FormValidation.error(e.getMessage());
+                    }
+                }
+            }
+
+            return FormValidation.ok();
+        }
     }
-  }
 }
